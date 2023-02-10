8ed8f9ad3a2a6bd8dab7c8097472f3a152e3bce3
kal <101020782+RoniKZX@users.noreply.github.com>
2023-02-10 14:42:08 -0400

    Add basic registration process

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2dab6e03ed72c36217d09d9c7dfca09a'
 
 posts = [
     {
@@ -26,7 +28,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -34,6 +36,19 @@ def hello():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run()
